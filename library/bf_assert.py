12889b0a93304dcf0b5ea509cb7f5d01c1dcb503
Spencer Fraint <spencer@intentionet.com>
2019-06-14 12:46:48 -0700

    Fail module on failed assert/validation (#14)
    * fail module on failed assert/validation

--- library/bf_assert.py
+++ library/bf_assert.py
@@ -85,10 +85,7 @@ summary:
     description: Summary of action(s) performed.
     type: str
 result:
-    description: List of high-level assertion results (name and status).
-    type: list
-result_verbose:
-    description: List of verbose assertion results, containing more details about why assertions failed.
+    description: List of assertion results.
     type: list
 '''
 
@@ -124,7 +121,6 @@ def run_module():
     result = dict(
         changed=False,
         result='',
-        result_verbose='',
         summary='',
     )
 
@@ -158,7 +154,6 @@ def run_module():
         module.exit_json(**result)
 
     results = []
-    results_verbose = []
     failed = []
     summary = 'Assertion(s) completed successfully'
 
@@ -190,22 +185,19 @@ def run_module():
 
         results.append({
             'name': assertion['name'],
-            'status': status,
-        })
-        results_verbose.append({
-            'name': assertion['name'],
             'type': assertion['type'],
             'status': status,
             'details': assert_result,
         })
     if failed:
         summary = '{} of {} assertions failed'.format(len(failed), len(assertions))
-        # Also add this as a warning that shows up in Ansible
-        result['warnings'] = [summary]
 
     result['summary'] = summary
     result['result'] = results
-    result['result_verbose'] = results_verbose
+    # Indicate failure to Ansible in the case of failed assert(s)
+    if failed:
+        module.fail_json(msg=summary, **result)
+
     module.exit_json(**result)
 
 def main():
