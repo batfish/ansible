0654b9b7ea2ba9ecf8b4b7559934b7ea81f8f901
Spencer Fraint <spencer@intentionet.com>
2019-06-18 13:49:48 -0700

    use conda

--- .buildkite/setup_ansible.sh
+++ .buildkite/setup_ansible.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Run everything in a virtual environment
-pip install virtualenv
-python -m virtualenv venv
-source venv/bin/activate
+# Setup conda so we can avoid permission issues setting up Python as non-root user
+curl -o conda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash conda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda create -y -n conda_env python=3.7
+source activate conda_env
 
 # Setup Ansible, and our Ansible role
 pip install ansible
