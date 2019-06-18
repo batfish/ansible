e93493b2d38ea4a90c5c44a37f0fbffa3f7d3fef
Spencer Fraint <spencer@intentionet.com>
2019-06-17 18:37:58 -0700

    assume latest version if none is specified

--- module_utils/bf_util.py
+++ module_utils/bf_util.py
@@ -108,10 +108,14 @@ def load_facts(input_directory):
             dict_ = yaml.safe_load(f.read())
             nodes, version = _unencapsulate_facts(dict_)
 
-            if version:
-                if out['version'] and version != out['version']:
-                    raise ValueError('Input file version mismatch!')
-                out['version'] = version
+            # Assume latest version if none is specified
+            if not version:
+                version = BATFISH_FACT_VERSION
+
+            # Make sure version is consistent
+            if out['version'] and version != out['version']:
+                raise ValueError('Input file version mismatch!')
+            out['version'] = version
 
             # We allow a single input file to specify more than one node
             for node in nodes:
@@ -133,7 +137,7 @@ def validate_facts(expected, actual):
                     'expected': expected_version,
                     'actual': actual_version
                 }
-            } for n in actual_facts
+            } for n in expected_facts
         }
     for node in actual_facts:
         if node in expected_facts:
@@ -286,8 +290,6 @@ def _encapsulate_nodes_facts(nodes_facts, version):
 def _unencapsulate_facts(facts, check_version=False):
     """Extract node facts and version from final fact format."""
     assert 'nodes' in facts, 'No nodes present in parsed facts file(s)'
-    if check_version:
-        assert 'version' in facts, 'No version present in parsed facts file(s)'
     return facts['nodes'], facts.get('version')
 
 
