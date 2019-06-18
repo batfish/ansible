0a3c212832e5699366daa0d22ca51753da2f6015
Spencer Fraint <spencer@intentionet.com>
2019-06-18 14:17:53 -0700

    skip prompting with extra var

--- .buildkite/run_ansible_tutorials.sh
+++ .buildkite/run_ansible_tutorials.sh
@@ -7,7 +7,7 @@ source ${BUILDKITE_DIR}/setup_ansible.sh
 pushd tutorials
 
 # Run tutorial setup
-ansible-playbook -i inventory playbooks/batfish_setup.yml
+ansible-playbook -i inventory --extra-vars "batch_mode=true" playbooks/batfish_setup.yml
 
 # Run the tutorial
 ansible-playbook -i inventory playbooks/tutorial1_extract_facts.yml
