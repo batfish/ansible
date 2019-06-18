b980bd8080aed202793a028b294d0d4b93ae3d51
Spencer Fraint <spencer@intentionet.com>
2019-06-18 16:40:33 -0700

    unpin regex version

--- .buildkite/setup_python.sh
+++ .buildkite/setup_python.sh
@@ -9,7 +9,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 conda create -y -n conda_env python=3.7
 source activate conda_env
 # Get around having to build regex since there is no linux wheel
-conda install --yes -c conda-forge regex=2019.02.18
+conda install --yes -c conda-forge regex
 
 pip install git+https://github.com/batfish/pybatfish.git
 pip install -r tutorials/requirements.txt
