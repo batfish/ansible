b463b49ea127c97a54318e3d8809395846d4605a
Spencer Fraint <spencer@intentionet.com>
2019-06-18 14:28:37 -0700

    force conda install

--- .buildkite/setup_ansible.sh
+++ .buildkite/setup_ansible.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Setup conda so we can avoid permission issues setting up Python as non-root user
 curl -o conda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash conda.sh -b -p $HOME/miniconda
+bash conda.sh -b -f -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda create -y -n conda_env python=3.7
 source activate conda_env
