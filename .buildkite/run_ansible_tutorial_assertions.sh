4da39a826c9ce7f4ecd286c0a47197617ff8180c
Spencer Fraint <spencer@intentionet.com>
2019-06-20 17:45:15 -0700

    fix dir

--- .buildkite/run_ansible_tutorial_assertions.sh
+++ .buildkite/run_ansible_tutorial_assertions.sh
@@ -6,9 +6,9 @@ source ${BUILDKITE_DIR}/setup_python.sh
 source ${BUILDKITE_DIR}/setup_ansible.sh
 
 pushd tutorials
-
 # Run setup playbook
 ansible-playbook -i inventory --extra-vars "batch_mode=True" playbooks/batfish_setup.yml
+popd
 
 # Run the tutorial assertions playbook, validating the output of the tutorials
 ansible-playbook -i inventory tests/e2e/test_tutorials.yml
