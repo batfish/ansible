f2d61ba61b10f8bda2692ecfb1fea90556158755
Spencer Fraint <spencer@intentionet.com>
2019-06-24 15:01:54 -0700

    install from github branch instead

--- .buildkite/setup_ansible.sh
+++ .buildkite/setup_ansible.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 
 pip install ansible
 
-# Install our local Ansible role instead of the one on galaxy
-ln -s $(pwd) $HOME/.ansible/roles/batfish.base
+ansible-galaxy install --force git+https://github.com/batfish/ansible.git,${BUILDKITE_BRANCH}
